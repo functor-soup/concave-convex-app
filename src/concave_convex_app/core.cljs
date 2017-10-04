2424ca9f93e18573e5f35096bf66d63829a969a0
luke <nerorevenge@gmail.com>
2017-10-04 10:56:53 -0400

    User is able to draw points  - lines between points are drawn automagically  - fuzzy matching with nearby points yet to be done

--- src/concave_convex_app/core.cljs
+++ src/concave_convex_app/core.cljs
@@ -1,5 +1,5 @@
 (ns concave-convex-app.core
-    (:require ))
+    (:require [quil.core :as q :include-macros true]))
 
 (enable-console-print!)
 
@@ -7,11 +7,48 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:points [{:x 166 :y 166} {:x 200 :y 300}]}))
 
+;; ================== utils ======================================
+
+(defn zipz [x]
+  (cond
+    (empty? x) []
+    (= (count x) 1) []
+    :else (let [p1 (butlast x)
+                p2 (rest x)]
+            (map vector p1 p2))))
+
+;; ===============================================================
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
 )
+
+(defn draw []
+    (q/background 200)
+    (q/fill 0)
+    (let [points (get @app-state :points)
+          zpoints (zipz points)]
+      (doseq [[z1 z2] zpoints]
+       (q/line (:x z1) (:y z1) (:x z2) (:y z2)))
+      (doseq [point points]
+        (q/ellipse (:x point) (:y point) 20 20))))
+
+(defn my-mouse-clicked []
+  (let [x (q/mouse-x)
+        y (q/mouse-y)
+        msg (str "Mouse was clicked at " x " and " y)]
+    (do
+       (swap! app-state update-in [:points] #(conj % {:x x :y y}))
+       (println msg) 
+      )))
+
+
+(q/defsketch hello
+    :draw draw
+    :host "chicken"
+    :size [500 500]
+    :mouse-clicked my-mouse-clicked)
